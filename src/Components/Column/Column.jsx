a47d060f8ea57ca3ea525497f48e131f5bfdbd86
Suryagayathri K P <suryagayathrikp@gmail.com>
2023-05-14 02:56:26 +0530

    re-designed Columns

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -1,118 +1,68 @@
 import "./Column.css";
 
-
 const Columns = () => {
   return (
     <div className="wrapper_Column">
-      <h2 className="h2_Column">COLUMNS</h2>
-      <div className="header_Column">
-        <div className="minus_Column">
-          <div className="minus1_Column">-</div>
-          <div className="minus2_Column">-</div>
-          <div className="minus3_Column">-</div>
-          <div className="minus4_Column">-</div>
-          <div className="minus5_Column">-</div>
+      <h2 className="h2_Column">CUSTOMIZE COLUMNS</h2>
+      <div className="child_wrap_Column">
+        <div className="header_Column">
+          <div className="head1_Column">
+            <div className="minus1_Column">-</div>
+            <div className="title_child_Column">SL.NO</div>
+          </div>
+          <div className="head2_Column">
+            <div className="minus2_Column">-</div>
+            <div className="title_child_Column">NAME</div>
+          </div>
+          <div className="head3_Column">
+            <div className="minus3_Column">-</div>
+            <div className="title_child_Column">EXPIRY</div>
+          </div>
+          <div className="head4_Column">
+            <div className="minus4_Column">-</div>
+            <div className="title_child_Column">MRP</div>
+          </div>
+          <div className="head5_Column">
+            <div className="minus5_Column">-</div>
+            <div className="bg_Column">
+              <div className="title_child5_Column">QTY</div>
+              <div className="plus_Column">+</div>
+            </div>
+          </div>
         </div>
-        <div className="container_Column">
-          <table className="title_Column">
-            <tr className="title_Column">
-              <td className="title_child_Column">SL.NO</td>
-              <td className="title_child_Column">NAME</td>
-              <td className="title_child_Column">EXPIRY</td>
-              <td className="title_child_Column">MRP</td>
-              <td className="title_child_Column">QTY</td>
-            </tr>
-          </table>
-          <div className="plus_Column">+</div>
-        </div>
+        <table className="table_Column">
+          <div className="odd_Column">
+            <div className="odd_child_Column">1</div>
+            <div className="even_child_Column">2</div>
+            <div className="odd_child_Column">3</div>
+            <div className="even_child_Column">4</div>
+          </div>
+          <div className="even_Column">
+            <div className="odd_child_Column">sugar</div>
+            <div className="even_child_Column">milk</div>
+            <div className="odd_child_Column">biscuit</div>
+            <div className="even_child_Column">boost</div>
+          </div>
+          <div className="odd_Column">
+            <div className="odd_child_Column">20/2/2024</div>
+            <div className="even_child_Column">20/2/2024</div>
+            <div className="odd_child_Column">20/2/2024</div>
+            <div className="even_child_Column">20/2/2024</div>
+          </div>
+          <div className="even_Column">
+            <div className="odd_child_Column">Rs.50</div>
+            <div className="even_child_Column">Rs.20</div>
+            <div className="odd_child_Column">Rs.50</div>
+            <div className="even_child_Column">Rs.20</div>
+          </div>
+          <div className="odd5_Column">
+            <div className="odd_child_Column">2.00</div>
+            <div className="even_child_Column">1.00</div>
+            <div className="odd_child_Column">2.00</div>
+            <div className="even_child_Column">1.00</div>
+          </div>
+        </table>
       </div>
-      <table className="table_Column">
-        <tr className="odd_Column">
-          <td className="odd_child_Column">
-            <input className="input_Column" type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-        </tr>
-        <tr className="even_Column">
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-        </tr>
-        <tr className="odd_Column">
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column" type="text"></input>
-          </td>
-        </tr>
-        <tr className="even_Column">
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="even_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-        </tr>
-        <tr className="odd_Column">
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-          <td className="odd_child_Column">
-            <input className="input_Column"  type="text"></input>
-          </td>
-        </tr>
-      </table>
     </div>
   );
 };
