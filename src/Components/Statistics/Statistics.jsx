a05fb54883db9013f4d035947b529b6d4f410b55
nikhitkumar <nikhitkumar00@gmail.com>
2023-05-14 10:48:17 +0530

    changed styles of statistics

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,4 +1,3 @@
-
 import "./Statistics.css";
 
 const Statistics = () => {
@@ -25,116 +24,58 @@ const Statistics = () => {
           </div>
         </div>
         <div className="tables_statistics">
-          <table className="purple_statistics bottom_ststistics">
+          <div className="flex_statistics">
             <div className="head_statistics">Status</div>
-            <tr>
-              <th className="th_statistics">SL No.</th>
-              <th className="th_statistics">Name</th>
-              <th className="th_statistics">Status</th>
-            </tr>
-            <tr>
-              <td className="td_statistics">1</td>
-              <td className="td_statistics">Product A</td>
-              <td className="td_statistics">80%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">2</td>
-              <td className="td_statistics">Product B</td> 
-              <td className="td_statistics">60%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">3</td> 
-              <td className="td_statistics">Product C</td>
-              <td className="td_statistics">90%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">4</td>
-              <td className="td_statistics">Product D</td>
-              <td className="td_statistics">75%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">5</td>
-              <td className="td_statistics">Product E</td>
-              <td className="td_statistics">85%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">6</td>
-              <td className="td_statistics">Product F</td>
-              <td className="td_statistics">50%</td> 
-            </tr>
-            <tr>
-              <td className="td_statistics">7</td>
-              <td className="td_statistics">Product G</td>
-              <td className="td_statistics">95%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">8</td>
-              <td className="td_statistics">Product H</td>
-              <td className="td_statistics">70%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">9</td>
-              <td className="td_statistics">Product I</td>
-              <td className="td_statistics">65%</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">10</td>
-              <td className="td_statistics">Product J</td>
-              <td className="td_statistics">88%</td>
-            </tr>
-          </table>
-          <table className="red_statistics bottom_ststistics">
-            <div className="head_statistics">Restock Items</div>
-            <tr>  
-              <th className="th_statistics">SL No.</th>
-              <th className="th_statistics">Name</th>
-            </tr>
-            <tr>
-              <td className="td_statistics">1</td>
-              <td className="td_statistics">Product A</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">2</td>
-              <td className="td_statistics">Product B</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">3</td>
-              <td className="td_statistics">Product C</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">4</td>
-              <td className="td_statistics">Product D</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">5</td>
-              <td className="td_statistics">Product E</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">6</td>
-              <td className="td_statistics">Product F</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">7</td>
-              <td className="td_statistics">Product G</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">8</td>
-              <td className="td_statistics">Product H</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">9</td>
-              <td className="td_statistics">Product I</td>
-            </tr>
-            <tr>
-              <td className="td_statistics">10</td>
-              <td className="td_statistics">Product J</td>
-            </tr>
-          </table>
+            <table className="purple_statistics bottom_ststistics">
+              <thead>
+                <tr>
+                  <th className="th_statistics">SL No.</th>
+                  <th className="th_statistics">Name</th>
+                  <th className="th_statistics">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="td_statistics">1</td>
+                  <td className="td_statistics">Product A</td>
+                  <td className="td_statistics">80%</td>
+                </tr>
+                <tr>
+                  <td className="td_statistics">2</td>
+                  <td className="td_statistics">Product B</td>
+                  <td className="td_statistics">60%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="tables_statistics">
+            <div className="flex_statistics">
+              <div className="head_statistics">Restock Items</div>
+              <table className="red_statistics bottom_ststistics">
+                <thead>
+                  <tr>
+                    <th className="th_statistics">SL No.</th>
+                    <th className="th_statistics">Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="td_statistics">1</td>
+                    <td className="td_statistics">Product A</td>
+                  </tr>
+                  <tr>
+                    <td className="td_statistics">2</td>
+                    <td className="td_statistics">Product B</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
       <div />
     </div>
   );
-}
+};
 
 export default Statistics;
