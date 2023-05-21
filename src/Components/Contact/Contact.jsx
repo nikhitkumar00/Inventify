ec71ca661cc307ae7215eaad564c2ce350c27b92
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-05-21 22:05:12 +0530

    added formspree api for contact us form submission

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,12 +5,13 @@ const Contact = () => {
         <div className="ContactMainContainer">
             <div className="ContactUsHeader">Contact Us</div>
             <div className="ContactFormContainer">
-                <form className='form_Contact'>
+                <form className='form_Contact' action="https://formspree.io/f/mzbqqbzg"
+  method="POST">
                     <div className="NameEmailContainer">
-                        <input type="text" placeholder="Name" className="UserName"/>
-                        <input type="text" placeholder="Email" className="Email" />
+                        <input type="text" placeholder="Name" className="UserName" name='Name'/>
+                        <input type="text" placeholder="Email" className="Email" name='Email' />
                     </div>
-                    <input type="text" placeholder="Content" className="Content" wrap=""/>
+                    <input type="text" placeholder="Content" className="Content" name='Content'/>
                     <div className="SubmitWrapper">
                     <button type="submit" className="SubmitButton">Submit</button>
 
