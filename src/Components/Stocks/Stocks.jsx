c3904887a8a838eff7968825f0a159f4c9a35a80
nikhitkumar <nikhitkumar00@gmail.com>
2023-05-21 18:03:22 +0530

    minor fixes

--- src/Components/Stocks/Stocks.jsx
+++ src/Components/Stocks/Stocks.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "./Stocks.css";
 
 export const Stocks = () => {
