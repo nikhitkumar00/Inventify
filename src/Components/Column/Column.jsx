a7dc96dca6863549e9f464b2577ec75547fd3fcc
nikhitkumar <nikhitkumar00@gmail.com>
2023-06-02 13:40:29 +0530

    added new column api

--- src/Components/Column/Column.jsx
+++ src/Components/Column/Column.jsx
@@ -1,6 +1,30 @@
 import "./Column.css";
+import { useState } from "react";
 
 const Columns = () => {
+  const [columnName, setColumnName] = useState("");
+  const [dataType, setDataType] = useState("");
+
+  const handleAddColumn = (e) => {
+    e.preventDefault();
+
+    const url = `http://127.0.0.1/I_N_V_O%20Backend/newcolumn.php?columnName=${columnName}&dataType=${dataType}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleColumnNameChange = (e) => {
+    setColumnName(e.target.value);
+    console.log(columnName);
+  };
+
   return (
     <div className="wrapper_Column">
       <h2 className="h2_Column">Customize Columns</h2>
@@ -27,56 +51,44 @@ const Columns = () => {
             <div className="title_child_Column">QTY</div>
           </div>
         </div>
-        <form className="table_Column">
+        <form className="table_Column" onSubmit={handleAddColumn}>
           <div className="container_Column">
             <input
               className="newColumn"
               type="text"
               placeholder="Column Name"
-            ></input>
+              value={columnName}
+              onChange={handleColumnNameChange}
+            />
             <div className="dropdown_Column">
-            <select className="type_Column" id="dropdown">
-                <option className="opt_Column" hidden selected>Data Type</option>
-                <option className="opt_Column" value="text">Text</option>
-                <option className="opt_Column" value="number">Number</option>
-                <option className="opt_Column" value="date">Date</option>
-            </select>
+              <select
+                className="type_Column"
+                id="dropdown"
+                value={dataType}
+                onChange={(e) => {
+                  setDataType(e.target.value);
+                  console.log(dataType)
+                }}
+              >
+                <option className="opt_Column" value="VARCHAR(40)">
+                  Text
+                </option>
+                <option className="opt_Column" value="INT">
+                  Number
+                </option>
+                <option className="opt_Column" value="DATE">
+                  Date
+                </option>
+                <option className="opt_Column" value="BOOLEAN">
+                  Boolean
+                </option>
+              </select>
             </div>
           </div>
-          <button className="btn_Column">Add Column</button>
+          <button className="btn_Column" type="submit">
+            Add Column
+          </button>
         </form>
-        {/*<form className="table_Column">
-          <div className="odd_Column">
-            <div className="odd_child_Column">1</div>
-            <div className="even_child_Column">2</div>
-            <div className="odd_child_Column">3</div>
-            <div className="even_child_Column">4</div>
-          </div>
-          <div className="even_Column">
-            <div className="odd_child_Column">sugar</div>
-            <div className="even_child_Column">milk</div>
-            <div className="odd_child_Column">biscuit</div>
-            <div className="even_child_Column">boost</div>
-          </div>
-          <div className="odd_Column">
-            <div className="odd_child_Column">20/2/2024</div>
-            <div className="even_child_Column">20/2/2024</div>
-            <div className="odd_child_Column">20/2/2024</div>
-            <div className="even_child_Column">20/2/2024</div>
-          </div>
-          <div className="even_Column">
-            <div className="odd_child_Column">Rs.50</div>
-            <div className="even_child_Column">Rs.20</div>
-            <div className="odd_child_Column">Rs.50</div>
-            <div className="even_child_Column">Rs.20</div>
-          </div>
-          <div className="odd5_Column">
-            <div className="odd_child_Column">2.00</div>
-            <div className="even_child_Column">1.00</div>
-            <div className="odd_child_Column">2.00</div>
-            <div className="even_child_Column">1.00</div>
-          </div>
-        </form>*/}
       </div>
     </div>
   );
