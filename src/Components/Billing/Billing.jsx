c99ecbea01f3f571ceda082b73b189902094309e
Krishnanand <krishnanandunnikrishnan@gmail.com>
2023-06-04 09:37:48 +0530

    Removed add container,added add button to product details container

--- src/Components/Billing/Billing.jsx
+++ src/Components/Billing/Billing.jsx
@@ -1,15 +1,75 @@
 import React from 'react'
 import './Billing.css'
 import  Add  from './Add'
-import  Productinfo  from './Productinfo'
+// import  Productinfo  from './Productinfo'
 import  Billingtable  from './Billingtable'
+
+var inputStyle={
+  input:{
+    border:"1px solid black",  
+    borderRadius:"10px",
+    padding:'15px',
+    margin:'10px',
+    fontSize:'15px',
+    backgroundColor: "var(--inputf)",
+    color: "var(--tar)",
+    placeholder: "var(--place)"
+  },
+ input1:{
+  border:"1px solid black",  
+  borderRadius:"10px",
+    padding:'15px',
+    flexGrow:'1',
+    margin:'10px',
+    backgroundColor: "var(--inputf)",
+    color: "var(--tar)",
+    fontSize:'15px',
+    maxWidth:'500px'
+
+
+
+ }
+} 
+
+//Billling Compon
 const Billing = () => {
   return (
     <div className='BillingContainer' >
       <div className='BillingHeader'>Billing</div>
       <div className='BillingContainerChild1'>
-        <Add />
-        <Productinfo className="Productinfo" />
+<div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: '2',
+            backgroundColor:'var(--sea)',
+            borderRadius:'10px',
+            marginRight:'20px',
+            padding:'10px',
+        }} >
+            <input type='text' placeholder='Name of the product' style={inputStyle.input} />
+            <div style={{
+                display:'flex'
+            }}>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <input type='text' placeholder='Serial No.'  style={inputStyle.input}/>
+                    <input type='text' placeholder='Qty'  style={inputStyle.input}/>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <input type='text' placeholder='Unit Price'  style={inputStyle.input}/>
+                    <input type='text' placeholder='Expiry'  style={inputStyle.input}/>
+                </div>
+                <div className='Inner_Child_1'>
+                <input type='input' placeholder='Total' style={inputStyle.input1}/>
+                <div className='Product_Add_Button_div'>
+                  <input type="button" value="Add " className='Product_Add_Button' />
+
+                </div>
+                </div>
+                
+            </div>
+
+        </div>
+
       </div>
       <Billingtable />
     </div>
--- src/Components/Billing/Productinfo.jsx
+++ src/Components/Billing/Productinfo.jsx
@@ -1,3 +1,4 @@
+import './Productinfo.css'
 import React from 'react'
 var inputStyle={
     input:{
@@ -19,36 +20,13 @@ var inputStyle={
       backgroundColor: "var(--inputf)",
       color: "var(--tar)",
       fontSize:'15px'
-    //   fontSize:'10px'
 
 
    }
   }
 export const Productinfo = () => {
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            flexGrow: '2',
-            backgroundColor:'var(--sea)',
-            borderRadius:'10px'
-        }} >
-            <input type='text' placeholder='Name of the product' style={inputStyle.input} />
-            <div style={{
-                display:'flex'
-            }}>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <input type='text' placeholder='Serial No.'  style={inputStyle.input}/>
-                    <input type='text' placeholder='Qty'  style={inputStyle.input}/>
-                </div>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <input type='text' placeholder='Unit Price'  style={inputStyle.input}/>
-                    <input type='text' placeholder='GST'  style={inputStyle.input}/>
-                </div>
-                <input type='input' placeholder='Total' style={inputStyle.input1}/>
-            </div>
-
-        </div>
+        
     )
 }
 
