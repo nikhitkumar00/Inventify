d05ca58f25fc23cf164704546cb5880764e9a0e4
Suryagayathri K P <suryagayathrikp@gmail.com>
2023-06-07 21:06:18 +0530

    billing input

--- src/Components/Billing/Billing.jsx
+++ src/Components/Billing/Billing.jsx
@@ -5,24 +5,25 @@ import "./Billing.css";
 
 const inputStyle = {
   input: {
-    border: "1px solid black",
-    borderRadius: "10px",
+    border: "none",
+    borderRadius : "10px",
     padding: "15px",
     margin: "10px",
     fontSize: "15px",
     backgroundColor: "var(--inputf)",
     color: "var(--tar)",
     placeholder: "var(--place)",
+
   },
   input1: {
-    border: "1px solid black",
-    borderRadius: "10px",
+    border: "none",
+    borderRadius : "10px",
     padding: "15px",
     flexGrow: "1",
     margin: "10px",
     backgroundColor: "var(--inputf)",
     color: "var(--tar)",
-    fontSize: "15px",
+    fontSize: "25px",
     maxWidth: "500px",
   },
 };
