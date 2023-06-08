205cedbb59401c86c6db56656600d5c79edebad3
nikhitkumar <nikhitkumar00@gmail.com>
2023-06-08 15:48:56 +0530

    added shell script to automatic setup environment

---
+++ INVO.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /opt/lampp && sudo ./xampp start
+
+cd ~
+
+cd ~/Desktop/I_N_V_O/
+
+npm run dev &
+
+sleep 1
+
+xdg-open http://localhost:5173
+
+read -n 1 -s -r -p "Press any key to stop XAMPP..."
+
+cd /opt/lampp && sudo ./xampp stop
+
